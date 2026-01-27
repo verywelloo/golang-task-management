b02721d4d66a832fbf2648d0b21b6a2e1160f3cb
Phoomsun Suthammatan <verywelloo@hotmail.com>
2026-01-27 14:53:46 +0700

    update initialize data

--- app/services/initial.go
+++ app/services/initial.go
@@ -1,6 +1,14 @@
 package service
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Service struct {
 	ShutdownCtx context.Context
@@ -9,7 +17,16 @@ type Service struct {
 var AppService *Service
 
 func InitializeData(ctx context.Context) error {
-	if err := initEnvironment(); err != nil {
+	// set mongo connection
+	db, err := initEnvironment()
+	if err != nil {
+		fmt.Printf("\n%v\n", err)
+		return err
+	}
+
+	// init collection
+	if err := InitCollection(db, ctx); err != nil {
+		fmt.Printf("\n%v\n", err)
 		return err
 	}
 
@@ -19,6 +36,44 @@ func InitializeData(ctx context.Context) error {
 	return nil
 }
 
-func initEnvironment() error {
+func initEnvironment() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// created client, open connection pool
+	client, err := mongo.Connect(
+		ctx, options.Client().ApplyURI(GetEnv("MONGOURI", "")), // set config
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// check connection
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, err // client = mongodb configs
+}
+
+func InitCollection(client *mongo.Client, ctx context.Context) error {
+	db := client.Database(GetEnv("DB_NAME", ""))
+
+	collections := []string{
+		"projects",
+	}
+
+	// create collection in mongo
+	for _, coll := range collections {
+		err := db.CreateCollection(ctx, coll)
+		if err != nil {
+			// skip duplicate error
+			if !mongo.IsDuplicateKeyError(err) && !strings.Contains(err.Error(), "NamespaceExists") {
+				fmt.Printf("\n%v\n", err)
+				return err
+			}
+		}
+	}
+
 	return nil
 }
