ac02e392bf904169ada8c3035f91634ddf2519a9
Phoomsun Suthammatan <verywelloo@hotmail.com>
2026-02-14 23:24:09 +0700

    outline auth middleware

---
+++ app/middleware/auth_middleware.go
@@ -0,0 +1,9 @@
+package middleware
+
+import "github.com/labstack/echo/v4"
+
+func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
